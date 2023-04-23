07bd8d03966200160d7f8cda7f59aca0f9d7211e
TECH-KASHIF <104705313+TECH-KASHIF@users.noreply.github.com>
2023-04-23 10:01:37 +0500

    Update SSB.py

--- SSB.py
+++ SSB.py
@@ -1,16 +1,1 @@
-import os, platform
-try:
-    import requests
-except:
-    os.system('pip install requests')
-import requests
-bit = platform.architecture()[0]
-if bit == '64bit':
-    from SSB import SSB
-    SSB()
-elif bit == '32bit':
-    from SSB import SSB
-    SSB()
-else:
-    print('\n YOUR DEVICE IS NOT SUPPORT THIS COMMAND')
-    os.system('exit')
+import SSB
